fe13192d9c9b96c8edc5b005fc5f94815a30b51a
Luan Schons Griebler <52085622+Luan1Schons@users.noreply.github.com>
2022-02-22 22:49:18 -0300

    Delete proxy.py

--- confs/proxy.py
+++
@@ -1,6 +0,0 @@
-class PROXY_INFO():
-
-    HTTP = 'http://oxy40578:TgqbUHIHUwnx@con.oxygate.io:4004'
-    HTTPS = 'http://oxy40578:TgqbUHIHUwnx@con.oxygate.io:4004'
-    PROXY_EXTENSION = 'oxy40578:TgqbUHIHUwnx@con.oxygate.io:4004'
-    NO_PROXY = 'dev.local:8080'
